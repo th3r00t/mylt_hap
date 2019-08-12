2eb6f1805fca4d397ddbe406ab56c165fdeb1cd1
Raelon Masters <admin@mylocaltech.co>
2019-08-12 15:49:05 -0400

    Moved interface to its own repo

--- interface/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.config.from_object('config')
-app.config["DEBUG"] = True
-@app.route("/")
-def hello():
-    return "Hello, Flask"
-if __name__ == "__main__":
-    app.run()
--- interface/wsgi.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0,"/srv/apps/")
-from mylt_hap import app as application
