2ce0d913180a97818c7a443ec1ee729d18e48ff9
Raelon Masters <admin@mylocaltech.co>
2019-08-15 16:02:21 -0400

    Updated firmware to toggle light & report state

--- firmware/src/wifi_connect.cpp
+++ firmware/src/wifi_connect.cpp
@@ -13,8 +13,10 @@ const char* mqtt_server = "10.0.0.2";
 const int timeZone = 1;
 const int timeOffset = -(4*60*60);
 int relayPin = 5;
+int lightState = 0;
 
-const char* hardwareId = "DINGUS_LAB_LIGHT";
+
+const char* hardwareId = "DINGUS_LAB_LIGHT_2";
 const char* hardwareName = "benchlight";
 
 WiFiUDP ntpUDP;
@@ -23,49 +25,63 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", timeOffset, 60000);
 PubSubClient mqttClient(wifi_client);
 
 long lastMsg = 0;
-const char* outTopic = "lighting";
-const char* inTopic = "lighting/benchlight";
+const char* announceTopic = "lighting";
+const char* topic = "lighting/benchlight";
 
 void mqttConnect() {
   while (!mqttClient.connected()) {
     digitalWrite(relayPin, HIGH);
     if (mqttClient.connect(hardwareId, "home", "A@2cb13")) {
       Serial.println("Connected");
-      mqttClient.publish(outTopic, hardwareName);
-      mqttClient.subscribe(inTopic);
+      mqttClient.publish(announceTopic, hardwareName);
+      mqttClient.subscribe(topic);
+      digitalWrite(relayPin, LOW);
     }
     else {
       Serial.print("mqtt connect failed, rc=");
       Serial.println(mqttClient.state());
-      delay(5000);
     }
   }
 }
 
+void toggleLight(){
+  Serial.println("toggleLight");
+  if (lightState == 0){
+    lightState = 1;
+    digitalWrite(relayPin, LOW);
+  }
+  else {
+    lightState = 0;
+    digitalWrite(relayPin, HIGH);
+  }
+}
+
+void lightStatus(){
+  if (lightState == 0){
+    mqttClient.publish(topic, "0");
+  }
+  else {
+    mqttClient.publish(topic, "1");
+  }
+}
+
 void callback(char* topic, byte* payload, unsigned int length){
-  /*DEBUG */
-  Serial.print("[");
-  Serial.print(topic);
-  Serial.print("] ");
   for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
     Serial.println();
-    if (i == 0 && payload[i] == '0')
-    {
-      digitalWrite(relayPin, LOW);
+    if (i == 0 && payload[i] == '0'){
+      toggleLight();
     }
-    else if (i == 0 && payload[i] == '1')
-    {
-      digitalWrite(relayPin, HIGH);
+    else if (i == 0 && payload[i] == '1'){
+      mqttClient.publish(topic, "1");
+      mqttClient.subscribe(topic);
     }
     else if (i == 0 && payload[i] == '2'){
-      mqttClient.publish(outTopic, "1");
-      mqttClient.subscribe(inTopic);
+      lightStatus();
     }
   }
-  /* DEBUG*/
   
-  mqttClient.subscribe(inTopic);
+  mqttClient.subscribe(topic);
 }
 // Begin Setup
 void setup() {
